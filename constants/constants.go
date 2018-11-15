7982861d646ec83bf7de11407d6c4488c0cb2bfa
Paweł Prażak <pawel.prazak@uk.tesco.com>
2018-11-15 09:08:41 +0100

    Update constants

--- constants/constants.go
+++ constants/constants.go
@@ -4,7 +4,7 @@ const (
 	// Name is the application name
 	Name = "crypt"
 	// Description  is s single line description of the application
-	Description = "Universal cryptographic tool with AWS KMS, GCP KMS and Azure Key Vault support"
+	Description = "Universal encryption and decryption tool with AWS KMS, GCP KMS and Azure Key Vault support"
 	// Author is the application author to display
 	Author = "VirtusLab"
 )
