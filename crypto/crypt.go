e0f18b415d43fc44403204f1f387071097e750c5
Tomasz Sęk <tomasz.sek.88@gmail.com>
2019-05-31 14:08:44 +0200

    Add EncryptFiles and DecryptFiles functions

--- crypto/crypt.go
+++ crypto/crypt.go
@@ -1,8 +1,12 @@
 package crypto
 
 import (
-	"github.com/VirtusLab/crypt/files"
+	"os"
+	"path"
+	"path/filepath"
 
+	"github.com/VirtusLab/go-extended/pkg/files"
+	"github.com/pkg/errors"
 	"github.com/sirupsen/logrus"
 )
 
@@ -10,7 +14,9 @@ import (
 type Crypt interface {
 	Crypter
 	EncryptFile(inputPath, outputPath string) error
+	EncryptFiles(inputDir, outputDir, inputExtension, outputExtension string) error
 	DecryptFile(inputPath, outputPath string) error
+	DecryptFiles(inputDir, outputDir, inputExtension, outputExtension string) error
 }
 
 // Crypter is an Encrypter and a Decrypter
@@ -38,6 +44,80 @@ type crypt struct {
 // New creates a new Crypt with the given provider
 func New(crypter Crypter) Crypt {
 	return &crypt{crypter: crypter}
+}
+
+func encryptOrDecryptFiles(
+	inputDir, outputDir, inputExtension, outputExtension string,
+	encryptDecryptFunc func(inputPath, outputPath string) error,
+	getTargetFileFunc func(file files.FileEntry, inputExtension, outputExtension string) files.FileEntry) error {
+	logrus.Infof("Directory mode selected: '%s' -> '%s'", inputDir, outputDir)
+
+	fileEntries, err := files.DirTree(inputDir)
+	if err != nil {
+		return errors.Wrapf(err, "can't scan the directory tree: '%s'", inputDir)
+	}
+
+	for _, file := range fileEntries {
+		if len(inputExtension) > 0 && inputExtension != file.Extension {
+			logrus.Debugf("Skipping '%s'", path.Join(file.Path, file.Name))
+			continue
+		}
+		logrus.Debugf("Processing '%s'", path.Join(file.Path, file.Name))
+
+		target := getTargetFileFunc(file, inputExtension, outputExtension)
+
+		rel, err := filepath.Rel(inputDir, file.Path)
+		if err != nil {
+			return errors.Wrapf(err, "can't get a relative path for: '%s'", file.Path)
+		}
+
+		target.Path = path.Join(outputDir, rel)
+
+		_, err = os.Stat(target.Path)
+		if os.IsNotExist(err) {
+			err := os.MkdirAll(target.Path, os.ModePerm)
+			if err != nil {
+				return errors.Wrapf(err, "can't create the target directory: '%s'", target.Path)
+			}
+			logrus.Infof("Target directory was created: '%s'", target.Path)
+		} else if err != nil {
+			return errors.Wrapf(err, "can't get file information for '%s'", target.Path)
+		}
+
+		err = encryptDecryptFunc(path.Join(file.Path, file.Name), path.Join(target.Path, target.Name))
+		if err != nil {
+			return errors.Wrap(err, "can't encrypt/decrypt a file")
+		}
+	}
+
+	return nil
+}
+
+func (c *crypt) EncryptFiles(inputDir, outputDir, inputExtension, outputExtension string) error {
+	targetFunc := func(file files.FileEntry, inputExtension, outputExtension string) files.FileEntry {
+		fileEntry := files.FileEntry{
+			Path: file.Path,
+			Name: file.Name + outputExtension,
+		}
+		fileEntry.Extension = filepath.Ext(fileEntry.Name)
+		return fileEntry
+	}
+	return encryptOrDecryptFiles(inputDir, outputDir, inputExtension, outputExtension, c.EncryptFile, targetFunc)
+}
+
+func (c *crypt) DecryptFiles(inputDir, outputDir, inputExtension, outputExtension string) error {
+	targetFunc := func(file files.FileEntry, inputExtension, outputExtension string) files.FileEntry {
+		if len(inputExtension) == 0 {
+			fileEntry := files.FileEntry{
+				Path: file.Path,
+				Name: file.Name + outputExtension,
+			}
+			fileEntry.Extension = filepath.Ext(fileEntry.Name)
+			return fileEntry
+		}
+		return files.TrimExtension(file, []string{inputExtension})
+	}
+	return encryptOrDecryptFiles(inputDir, outputDir, inputExtension, outputExtension, c.DecryptFile, targetFunc)
 }
 
 // EncryptFile encrypts bytes from a file or stdin using a Crypter provider
--- crypto/crypt_test.go
+++ crypto/crypt_test.go
@@ -3,15 +3,17 @@ package crypto
 import (
 	"io/ioutil"
 	"os"
+	"path"
 	"testing"
 
 	"github.com/VirtusLab/crypt/test/fake"
 
+	"github.com/VirtusLab/go-extended/pkg/files"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
-func when(crypt Crypt, inputPath string) (string, error) {
+func encryptDecryptSingleFile(crypt Crypt, inputPath string) (string, error) {
 	defer func() { _ = os.Remove(inputPath + ".encrypted") }() // clean up
 	defer func() { _ = os.Remove(inputPath + ".decrypted") }() // clean up
 
@@ -34,8 +36,7 @@ func when(crypt Crypt, inputPath string) (string, error) {
 }
 
 func TestCrypt_EncryptDecryptFile(t *testing.T) {
-	kms := fake.Empty()
-	crypt := New(kms)
+	crypt := New(fake.Empty())
 
 	inputFile := "test.txt"
 	expected := "top secret token"
@@ -43,19 +44,69 @@ func TestCrypt_EncryptDecryptFile(t *testing.T) {
 	require.NoError(t, err, "Can't write plaintext file")
 	defer func() { _ = os.Remove(inputFile) }()
 
-	actual, err := when(crypt, inputFile)
+	actual, err := encryptDecryptSingleFile(crypt, inputFile)
 
 	assert.NoError(t, err)
 	assert.Equal(t, expected, string(actual))
 }
 
 func TestCrypt_EncryptDecryptFileError(t *testing.T) {
-	kms := fake.Empty()
-	crypt := New(kms)
+	crypt := New(fake.Empty())
 
 	inputFile := "test.txt"
 
-	_, err := when(crypt, inputFile)
+	_, err := encryptDecryptSingleFile(crypt, inputFile)
 
 	assert.EqualError(t, err, "open test.txt: no such file or directory")
+}
+
+func TestCrypt_EncryptDecryptFiles(t *testing.T) {
+	crypt := New(fake.Empty())
+	encryptedFileExtension := ".crypt"
+	rootFileName := "root.txt"
+	subdirectoryFileName := "sub-directory.txt"
+	subdirectoryName := "sub-directory"
+	inDir := "testdata/encryptDecryptFiles"
+	encryptedFilesDir := "encryptedFiles"
+	decryptedFilesDir := "decryptedFiles"
+
+	defer func() { _ = os.RemoveAll(encryptedFilesDir) }()
+	err := crypt.EncryptFiles(inDir, encryptedFilesDir, "", encryptedFileExtension)
+	require.NoError(t, err)
+	assert.FileExists(t, path.Join(encryptedFilesDir, rootFileName+encryptedFileExtension))
+	assert.FileExists(t, path.Join(encryptedFilesDir, subdirectoryName, subdirectoryFileName+encryptedFileExtension))
+
+	defer func() { _ = os.RemoveAll(decryptedFilesDir) }()
+	err = crypt.DecryptFiles(encryptedFilesDir, decryptedFilesDir, encryptedFileExtension, "")
+	require.NoError(t, err)
+	assert.FileExists(t, path.Join(decryptedFilesDir, rootFileName))
+	assert.FileExists(t, path.Join(decryptedFilesDir, subdirectoryName, subdirectoryFileName))
+
+	rootFile, err := files.ReadInput(path.Join(inDir, rootFileName))
+	require.NoError(t, err)
+	rootFileAfterDecryption, err := files.ReadInput(path.Join(decryptedFilesDir, rootFileName))
+	require.NoError(t, err)
+	assert.Equal(t, rootFile, rootFileAfterDecryption)
+
+	subdirectoryFile, err := files.ReadInput(path.Join(inDir, subdirectoryName, subdirectoryFileName))
+	require.NoError(t, err)
+	subdirectoryFileAfterDecryption, err := files.ReadInput(path.Join(decryptedFilesDir, subdirectoryName, subdirectoryFileName))
+	require.NoError(t, err)
+	assert.Equal(t, subdirectoryFile, subdirectoryFileAfterDecryption)
+}
+
+func TestCrypt_DecryptFiles(t *testing.T) {
+	crypt := New(fake.Empty())
+	encryptedFileExtension := ".crypt"
+	rootFileName := "root.txt"
+	skipMeFileName := "skip-me.txt"
+	encryptedFilesDir := "testdata/decryptFiles"
+	decryptedFilesDir := "decryptedFiles"
+
+	defer func() { _ = os.RemoveAll(decryptedFilesDir) }()
+	err := crypt.DecryptFiles(encryptedFilesDir, decryptedFilesDir, encryptedFileExtension, "")
+	require.NoError(t, err)
+	assert.FileExists(t, path.Join(decryptedFilesDir, rootFileName))
+	_, err = os.Lstat(path.Join(decryptedFilesDir, skipMeFileName))
+	assert.EqualError(t, err, "lstat decryptedFiles/skip-me.txt: no such file or directory")
 }
